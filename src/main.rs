c389681d836efba70357c0353158282b4fdb5f77
ZYJ-q <2979049853@qq.com>
2023-06-13 13:43:01 +0800

    l

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,8 @@ async fn real_time(
                 let res_equity = trade_mapper::TradeMapper::get_equity();
                 if let Ok(e) = res_equity {
                     for eq in e {
-                        println!("eqqqqqqqqqqqq{}, name{}", eq.name, name);
+                        let value: Value = serde_json::from_str(&eq.name).unwrap();
+                        println!("eqqqqqqqqqqqq{}, name{}", value, name);
                     }
                 }
 
